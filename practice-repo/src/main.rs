6b4f732cad17f3aa87d5ceb53bb3b78b1d7da21e
Ayrnen <ayrnen@pm.me>
2025-10-25 11:14:10 -0400

    cargo run

--- practice-repo/src/main.rs
+++ practice-repo/src/main.rs
@@ -1,57 +1,44 @@
-fn gcd(mut n:i32, mut m:i32) -> i32 {
+fn gcd(mut n: i32, mut m: i32) -> i32 {
     assert!(n != 0 && m != 0);
     while m != 0 {
-        //exchange the value of m and n if m > n
         if m < n {
-            let t = m;
-            m = n;
-            n = t;
+            std::mem::swap(&mut m, &mut n);
         }
-        
-        m %= n; 
-
+        m %= n;
     }
-    
-    n // never put semicolon when returning a value 
+    n
 }
 
 #[test]
-fn test_gcd(){
+fn test_gcd() {
     assert_eq!(gcd(14, 15), 1);
-    assert_eq!(gcd(2*3*5*11*17, 3*7*11*13*19), 3*11);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
 }
-
-// use std::env;
 
 use std::str::FromStr;
 
 fn main() {
-    // declare new vector (dynamic array)
-    let mut numbers = Vec::new();
-    // grab args from cmd - skipping the first one (file loc)
-    let args = std::env::args().skip(1);
-    for arg in args {
-        //convert args from strings to numbers
-        let res = i32::from_str(&arg);
-        match {
-            Ok(num) => {
-                numbers.push(num);
-            }
+    let mut numbers: Vec<i32> = Vec::new();
+
+    for arg in std::env::args().skip(1) {
+        match i32::from_str(&arg) {
+            Ok(num) => numbers.push(num),
             Err(e) => {
                 eprintln!("Arg/Parsing error: {}", e);
                 std::process::exit(1);
             }
         }
     }
-    if numbers.len() == 0 {
+
+    if numbers.is_empty() {
         eprintln!("Usage: gcd num1 num2 ...");
-        std::process::exit(code:1);
+        std::process::exit(1);
     }
 
     let mut d = numbers[0];
-    for m &numbers[1..] {
+    for m in &numbers[1..] {
         d = gcd(d, *m);
     }
 
-    println!("the greatest common devisor of {:?} is {}", numbers, d)
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
---
+++ practice-repo/target/debug/deps/practice_repo-9b921c4f9c6c94aa.d
@@ -0,0 +1,5 @@
+/Users/aymenbelakhoua/Projects/Rust Coursera/practice-repo/target/debug/deps/practice_repo-9b921c4f9c6c94aa.d: src/main.rs
+
+/Users/aymenbelakhoua/Projects/Rust Coursera/practice-repo/target/debug/deps/practice_repo-9b921c4f9c6c94aa: src/main.rs
+
+src/main.rs:
