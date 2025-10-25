80716fb5bb7a10bdbaf6802acc1dc29a399af27b
Ayrnen <ayrnen@pm.me>
2025-10-25 12:17:41 -0400

    closure completed

--- practice-three/src/main.rs
+++ practice-three/src/main.rs
@@ -1,3 +1,13 @@
+fn func_with_closure<G>(f: G) where G:FnOnce(&str), {
+    f("hello world!");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let s: &str = "The content of x is: ";
+    let print_s_closure = |x:&str|{
+        println!("{} {}", s, x);    
+    };
+
+    func_with_closure(print_s_closure);
+
 }
