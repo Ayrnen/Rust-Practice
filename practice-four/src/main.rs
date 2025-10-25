cd9f40ce2b257580290f2f890dfe078ab4ff62a7
Ayrnen <ayrnen@pm.me>
2025-10-25 13:07:18 -0400

    Completed HTTP practice

--- practice-four/src/main.rs
+++ practice-four/src/main.rs
@@ -1,14 +1,35 @@
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
-use serde::ser;
+use actix_web::{dev::Server, web::{self, post}, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParameters{
+    n: i32,
+    m: i32,
+}
+
+fn gcd(mut n:i32, mut m:i32) -> i32 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        //exchange the value of m and n if m > n
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        
+        m %= n;
+
+    }
+    
+    n // never put semicolon when returning a value 
+}
 
 #[actix_web::main]
 async fn main() {
     let server_closure = ||{
         App::new()
-            .route(
-                "/",
-                web::get().to(get_index)
-            ) 
+            .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
     };
 
     let server = HttpServer::new(server_closure);
@@ -25,5 +46,28 @@ async fn main() {
 async fn get_index() -> HttpResponse {
     HttpResponse::Ok()
         .content_type("text/html")
-        .body("It works!")
+        .body(
+            r#"
+            <title>GCD Calculator</title>
+            <form action="/gcd" method="post">
+            <input type="text" name="n"/>
+            <input type="text" name="m"/>
+            <button type="submit">Compute GCD</button>
+            </form>
+            "#,
+        )
+}
+
+async fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing GCD with 0 is impossible.");
+    }
+    
+    let response = format!("The GCD of {} and {} is {}", form.n, form.m, gcd(form.n, form.m));
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
 }
