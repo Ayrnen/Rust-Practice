653ef8239b11f9bd36f283822d830fed5799d9bb
Ayrnen <ayrnen@pm.me>
2025-10-25 12:47:40 -0400

    Clean up formatting

--- practice-four/src/main.rs
+++ practice-four/src/main.rs
@@ -4,16 +4,26 @@ use serde::ser;
 #[actix_web::main]
 async fn main() {
     let server_closure = ||{
-        App::new().route("/", web::get().to(get_index)) 
+        App::new()
+            .route(
+                "/",
+                web::get().to(get_index)
+            ) 
     };
 
     let server = HttpServer::new(server_closure);
     println!("Server is listening on http://localhost:3000...");
 
-    server.bind("127.0.0.1:3000").expect("Error binding server to")
-    .run().await.expect("error running the server")
+    server
+        .bind("127.0.0.1:3000")
+        .expect("Error binding server to")
+        .run()
+        .await
+        .expect("error running the server")
 }
 
 async fn get_index() -> HttpResponse {
-    HttpResponse::Ok().content_type("text/html").body("It works!")
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body("It works!")
 }
