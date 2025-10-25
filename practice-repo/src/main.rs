feca737fbd34cc3e31a52dacb5d05f9504624925
Ayrnen <ayrnen@pm.me>
2025-10-25 11:15:02 -0400

    go back to original code

--- practice-repo/src/main.rs
+++ practice-repo/src/main.rs
@@ -1,38 +1,51 @@
-fn gcd(mut n: i32, mut m: i32) -> i32 {
+fn gcd(mut n:i32, mut m:i32) -> i32 {
     assert!(n != 0 && m != 0);
     while m != 0 {
+        //exchange the value of m and n if m > n
         if m < n {
-            std::mem::swap(&mut m, &mut n);
+            let t = m;
+            m = n;
+            n = t;
         }
-        m %= n;
+        
+        m %= n; 
+
     }
-    n
+    
+    n // never put semicolon when returning a value 
 }
 
 #[test]
-fn test_gcd() {
+fn test_gcd(){
     assert_eq!(gcd(14, 15), 1);
-    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
+    assert_eq!(gcd(2*3*5*11*17, 3*7*11*13*19), 3*11);
 }
+
+// use std::env;
 
 use std::str::FromStr;
 
 fn main() {
-    let mut numbers: Vec<i32> = Vec::new();
-
-    for arg in std::env::args().skip(1) {
-        match i32::from_str(&arg) {
-            Ok(num) => numbers.push(num),
+    // declare new vector (dynamic array)
+    let mut numbers = Vec::new();
+    // grab args from cmd - skipping the first one (file loc)
+    let args = std::env::args().skip(1);
+    for arg in args {
+        //convert args from strings to numbers
+        let res = i32::from_str(&arg);
+        match {
+            Ok(num) => {
+                numbers.push(num);
+            }
             Err(e) => {
                 eprintln!("Arg/Parsing error: {}", e);
                 std::process::exit(1);
             }
         }
     }
-
-    if numbers.is_empty() {
+    if numbers.len() == 0 {
         eprintln!("Usage: gcd num1 num2 ...");
-        std::process::exit(1);
+        std::process::exit(code:1);
     }
 
     let mut d = numbers[0];
@@ -40,5 +53,5 @@ fn main() {
         d = gcd(d, *m);
     }
 
-    println!("The greatest common divisor of {:?} is {}", numbers, d);
+    println!("the greatest common devisor of {:?} is {}", numbers, d)
 }
